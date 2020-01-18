5d903d87fc1e5e58a7d1ea6e5d0fd9cf49e27461
hsz <shengzhong.huang@zhengmen.com>
2020-01-18 14:37:26 +0800

    carthage test

--- SZPublicPodTest-frameworkTests/SZPublicPodTest_frameworkTests.m
+++ SZPublicPodTest-frameworkTests/SZPublicPodTest_frameworkTests.m
@@ -1,19 +1,19 @@
 //
-//  SZPublicPodUITestTests.m
-//  SZPublicPodUITestTests
+//  SZPublicPodTest_frameworkTests.m
+//  SZPublicPodTest-frameworkTests
 //
-//  Created by hsz on 2020/1/9.
+//  Created by hsz on 2020/1/18.
 //  Copyright © 2020 hsz. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
 #import "SZTest.h"
 
-@interface SZPublicPodUITestTests : XCTestCase
+@interface SZPublicPodTest_frameworkTests : XCTestCase
 
 @end
 
-@implementation SZPublicPodUITestTests
+@implementation SZPublicPodTest_frameworkTests
 
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -26,8 +26,8 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    [SZTest1 addATestRedViewInView:[UIView new] frame:CGRectMake(0, 0, 100, 100)];
-    [SZTest2 addATestBlueViewInView:[UIView new] frame:CGRectMake(0, 100, 100, 100)];
+    [SZTest1 addATestRedViewInView:[UIView new] frame:CGRectMake(100, 100, 100, 100)];
+    [SZTest2 addATestBlueViewInView:[UIView new] frame:CGRectMake(100, 200, 100, 100)];
 }
 
 - (void)testPerformanceExample {
--- SZPublicPodTest.podspec
+++ SZPublicPodTest.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'SZPublicPodTest'
-  s.version          = '0.1.6'
+  s.version          = '0.1.7'
   s.summary          = 'what the fuck.'
 
 # This description is used to generate tags and improve search results.
